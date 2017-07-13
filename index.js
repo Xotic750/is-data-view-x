170b991a7fa31080690cc106bec3da4e196e8df7
Xotic750 <xotic750@gmail.com>
2017-07-13 13:02:17 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,108 +1,80 @@
 /**
- * @file
- * <a href="https://travis-ci.org/Xotic750/is-data-view-x"
- * title="Travis status">
- * <img src="https://travis-ci.org/Xotic750/is-data-view-x.svg?branch=master"
- * alt="Travis status" height="18">
- * </a>
- * <a href="https://david-dm.org/Xotic750/is-data-view-x"
- * title="Dependency status">
- * <img src="https://david-dm.org/Xotic750/is-data-view-x.svg"
- * alt="Dependency status" height="18"/>
- * </a>
- * <a
- * href="https://david-dm.org/Xotic750/is-data-view-x#info=devDependencies"
- * title="devDependency status">
- * <img src="https://david-dm.org/Xotic750/is-data-view-x/dev-status.svg"
- * alt="devDependency status" height="18"/>
- * </a>
- * <a href="https://badge.fury.io/js/is-data-view-x" title="npm version">
- * <img src="https://badge.fury.io/js/is-data-view-x.svg"
- * alt="npm version" height="18">
- * </a>
- *
- * Detect whether or not an object is a DataView.
- *
- * Requires ES3 or above.
- *
- * @version 1.2.1
+ * @file Detect whether or not an object is a DataView.
+ * @version 1.3.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module is-data-view-x
  */
 
-/* eslint strict: 1, max-statements: 1 */
+/* global ArrayBuffer, DataView */
 
-/* global module, DataView, ArrayBuffer */
+'use strict';
 
-;(function () { // eslint-disable-line no-extra-semi
+var isObjectLike = require('is-object-like-x');
+var hasDView = typeof DataView === 'function';
+var getByteLength;
+var legacyCheck;
 
-  'use strict';
-
-  var isObjectLike = require('is-object-like-x');
-  var hasDView = typeof DataView === 'function';
-  var getByteLength;
-  var legacyCheck;
-
-  if (hasDView) {
-    if (require('has-to-string-tag-x')) {
-      try {
-        getByteLength = Object.getOwnPropertyDescriptor(
-          DataView.prototype,
-          'byteLength'
-        ).get;
-        getByteLength = typeof getByteLength.call(
-          new DataView(new ArrayBuffer(4))
-        ) !== 'number' && getByteLength;
-      } catch (ignore) {
-        getByteLength = null;
-      }
-    }
-    if (!getByteLength) {
-      var toStringTag = require('to-string-tag-x');
-      var dViewTag = '[object DataView]';
-      if (toStringTag(new DataView(new ArrayBuffer(4))) === dViewTag) {
-        legacyCheck = function byStringTag(object) {
-          return toStringTag(object) === dViewTag;
-        };
-      } else {
-        var isArrayBuffer = require('is-array-buffer-x');
-        legacyCheck = function byDuckType(object) {
-          var isByteLength = typeof object.byteLength === 'number';
-          var isByteOffset = typeof object.byteOffset === 'number';
-          var isGetFloat32 = typeof object.getFloat32 === 'function';
-          var isSetFloat64 = typeof object.setFloat64 === 'function';
-          return isByteLength && isByteOffset && isGetFloat32 && isSetFloat64 && isArrayBuffer(object.buffer);
-        };
-      }
-    }
+if (hasDView) {
+  if (require('has-to-string-tag-x')) {
+    try {
+      getByteLength = Object.getOwnPropertyDescriptor(
+        DataView.prototype,
+        'byteLength'
+      ).get;
+      getByteLength = typeof getByteLength.call(
+        new DataView(new ArrayBuffer(4))
+      ) !== 'number' && getByteLength;
+    } catch (ignore) {}
   }
 
-  /**
-   * Determine if an `object` is an `DataView`.
-   *
-   * @param {*} object The object to test.
-   * @return {boolean} `true` if the `object` is a `DataView`, else `false`.
-   * @example
-   * var isDataView = require('is-data-view-x');
-   * var ab = new ArrayBuffer(4);
-   * var dv = new DataView(ab);
-   *
-   * isDataView(ab); // false
-   * isDataView(true); // false
-   * isDataView(dv); // true
-   */
-  module.exports = function isDataView(object) {
-    if (!hasDView || !isObjectLike(object)) {
-      return false;
+  if (Boolean(getByteLength) === false) {
+    var toStringTag = require('to-string-tag-x');
+    var dViewTag = '[object DataView]';
+    if (toStringTag(new DataView(new ArrayBuffer(4))) === dViewTag) {
+      legacyCheck = function byStringTag(object) {
+        return toStringTag(object) === dViewTag;
+      };
+    } else {
+      var isArrayBuffer = require('is-array-buffer-x');
+      legacyCheck = function byDuckType(object) {
+        var isByteLength = typeof object.byteLength === 'number';
+        var isByteOffset = typeof object.byteOffset === 'number';
+        var isGetFloat32 = typeof object.getFloat32 === 'function';
+        var isSetFloat64 = typeof object.setFloat64 === 'function';
+        return isByteLength && isByteOffset && isGetFloat32 && isSetFloat64 && isArrayBuffer(object.buffer);
+      };
     }
-    if (legacyCheck) {
-      return legacyCheck(object);
-    }
-    try {
-      return typeof getByteLength.call(object) === 'number';
-    } catch (ignore) {}
+  }
+}
+
+/**
+ * Determine if an `object` is an `DataView`.
+ *
+ * @param {*} object - The object to test.
+ * @returns {boolean} `true` if the `object` is a `DataView`, else `false`.
+ * @example
+ * var isDataView = require('is-data-view-x');
+ * var ab = new ArrayBuffer(4);
+ * var dv = new DataView(ab);
+ *
+ * isDataView(ab); // false
+ * isDataView(true); // false
+ * isDataView(dv); // true
+ */
+module.exports = function isDataView(object) {
+  if (hasDView === false || isObjectLike(object) === false) {
     return false;
-  };
-}());
+  }
+
+  if (legacyCheck) {
+    return legacyCheck(object);
+  }
+
+  try {
+    return typeof getByteLength.call(object) === 'number';
+  } catch (ignore) {}
+
+  return false;
+};
