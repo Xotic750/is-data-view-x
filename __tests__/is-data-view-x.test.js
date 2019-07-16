0e6971cff4afcf995af7813c433f4bfc57c831fe
Graham Fairweather <xotic750@gmail.com>
2019-07-16 22:46:06 +0200

    Terraformed
    Auto build

--- __tests__/is-data-view-x.test.js
+++ __tests__/is-data-view-x.test.js
@@ -4,29 +4,6 @@
 
 let isDataView;
 
-if (typeof module === 'object' && module.exports) {
-  require('es5-shim');
-  require('es5-shim/es5-sham');
-
-  if (typeof JSON === 'undefined') {
-    JSON = {};
-  }
-
-  require('json3').runInContext(null, JSON);
-  require('es6-shim');
-  const es7 = require('es7-shim');
-  Object.keys(es7).forEach(function(key) {
-    const obj = es7[key];
-
-    if (typeof obj.shim === 'function') {
-      obj.shim();
-    }
-  });
-  isDataView = require('../../index.js');
-} else {
-  isDataView = returnExports;
-}
-
 const ifHasDataView = typeof DataView === 'function' ? it : xit;
 
 describe('isDataView', function() {
