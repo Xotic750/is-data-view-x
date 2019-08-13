d4336d0db707d018afa5b5b5521133da320b8ce1
Graham Fairweather <xotic750@gmail.com>
2019-08-13 17:42:26 +0200

    Auto run

--- dist/is-data-view-x.js
+++ dist/is-data-view-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-13T12:01:00.435Z",
+  "date": "2019-08-13T15:42:20.707Z",
   "describe": "",
   "description": "Detect whether or not an object is a DataView.",
   "file": "is-data-view-x.js",
