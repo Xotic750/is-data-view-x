87e6b5172862dee5cdbda524804b3ecce4cabed8
Graham Fairweather <xotic750@gmail.com>
2019-09-05 22:25:10 +0200

    Maintenance

--- dist/is-data-view-x.js
+++ dist/is-data-view-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-09-05T00:13:11.227Z",
+  "date": "2019-09-05T20:25:04.013Z",
   "describe": "",
   "description": "Detect whether or not an object is a DataView.",
   "file": "is-data-view-x.js",
